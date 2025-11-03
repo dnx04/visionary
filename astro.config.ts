2c2800f48e07b5b6f60ad7d63fc3137a5a855599
Duong Nguyen <dnx04.gg@gmail.com>
2025-11-03 19:30:42 +0700

    testing github actions and pages

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://dnx04.github.io',
-  base: '/visionary',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
