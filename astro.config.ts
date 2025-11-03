0a1eb9cf10c6fe5ea363b59141238f7042f7f359
Duong Nguyen <dnx04.gg@gmail.com>
2025-11-03 19:24:44 +0700

    testing github actions and pages

--- astro.config.ts
+++ astro.config.ts
@@ -19,7 +19,8 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://astro-erudite.vercel.app',
+  site: 'https://dnx04.github.io',
+  base: '/visionary',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
